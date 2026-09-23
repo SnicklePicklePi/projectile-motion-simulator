e5682ebd9e78a6eb43cab921d3bcde4633df4c14
SnicklePicklePi <sophie.thirry@gmail.com>
2026-09-22 20:26:29 -0400

    Update projectile.py

--- projectile.py
+++ projectile.py
@@ -3,7 +3,9 @@ import math
 velocity = 20
 angle = 45
 gravity = 9.81
+
 vx = velocity * math.cos(math.radians(angle))
 vy = velocity * math.sin(math.radians(angle))
+
 print("Horizontal velocity:", vx)
 print("Vertical velocity:", vy)
